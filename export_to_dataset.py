329e2af9ca77c039727e3e8cf20bc0feab9eb4d3
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-25 23:01:28 +0800

    Add scripts to preprocess data

--- export_to_dataset.py
+++ export_to_dataset.py
@@ -2,14 +2,20 @@ import argparse
 import json
 import os
 import logging
+import re
 
-from utils import save_json
+from utils import (
+    save_json,
+    is_good_text,
+    unescape_string,
+)
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-def generate_dataset(data_file_name):
+
+def generate_dataset(output_dir, data_file_name, do_preprocess):
     dataset_directory = os.path.join("saves", data_file_name, "answers")
     qa_filenames = os.listdir(dataset_directory)
     dataset = []
@@ -17,19 +23,23 @@ def generate_dataset(data_file_name):
         with open(os.path.join(dataset_directory, qa_filename), "r") as json_file:
             qa_list = json.load(json_file)
             for qa in qa_list:
-                question = qa.get("问题","")
-                answer = qa.get("回答","")
-                if question and answer:
-                    dataset.append(
-                        {
-                            "prompt": question,
-                            "query": "",
-                            "response": answer,
-                            "history": [],
-                        }
-                    )
-    os.makedirs("datasets", exist_ok=True)
-    save_json(f"datasets/{data_file_name}_dataset.json", dataset)
+                question = unescape_string(qa.get("问题",""))
+                answer = unescape_string(qa.get("回答",""))
+
+                if do_preprocess and ((not is_good_text(question)) \
+                                  or (not is_good_text(answer))):
+                    continue
+                dataset.append(
+                    {
+                        "prompt": question,
+                        "query": "",
+                        "response": answer,
+                        "history": [],
+                    }
+                )
+
+    os.makedirs(output_dir, exist_ok=True)
+    save_json(f"{output_dir}/{data_file_name}_dataset.json", dataset)
     logger.info(f"Generated dataset for {data_file_name} successfully. (Total: {len(dataset)} samples)")
 
 if __name__ == "__main__":
@@ -37,5 +47,9 @@ if __name__ == "__main__":
     parser.add_argument("--data_file_name", type=str, required=True,
                         choices=["mathematical_statistics", "statistics", "deeplearning", "machine_learning"],
                         help="The name of the data file.")   
+    parser.add_argument("--output_dir", type=str, default="datasets",
+                        help="The output directory for the generated dataset.")
+    parser.add_argument("--do_preprocess", action="store_true",
+                        help="Whether to preprocess the data before generating the dataset.")
     args = parser.parse_args()
-    generate_dataset(args.data_file_name)
+    generate_dataset(args.output_dir, args.data_file_name, args.do_preprocess)
---
+++ preprocess_dataset.sh
@@ -0,0 +1,12 @@
+python ./export_to_dataset.py --data_file_name statistics \
+                              --output_dir preprocessed_datasets \
+                              --do_preprocess
+python ./export_to_dataset.py --data_file_name mathematical_statistics \
+                              --output_dir preprocessed_datasets \
+                              --do_preprocess
+python ./export_to_dataset.py --data_file_name deeplearning \
+                              --output_dir preprocessed_datasets \
+                              --do_preprocess
+python ./export_to_dataset.py --data_file_name machine_learning \
+                              --output_dir preprocessed_datasets \
+                              --do_preprocess
