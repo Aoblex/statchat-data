a19f2b8925e45c7ab0feb746ba2150151d407f78
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-25 19:17:14 +0800

    Corrected generation script

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,4 +1,4 @@
-pytyon ./export_to_dataset.py --data_file_name statistics
-pytyon ./export_to_dataset.py --data_file_name mathematical_statistics
+python ./export_to_dataset.py --data_file_name statistics
+python ./export_to_dataset.py --data_file_name mathematical_statistics
 python ./export_to_dataset.py --data_file_name deeplearning
 python ./export_to_dataset.py --data_file_name machine_learning 
