49bc75bb5044f2239061a652565f2580a21d6b5b
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-25 23:01:01 +0800

    Check data automatically

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import re
+import markdown
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,7 @@ escape_dict = {
     "\x08": "\\b",  # ASCII 08
     "\x0b": "\\v",  # ASCII 11
     "\x0c": "\\f",  # ASCII 12
+    "\\\\":"\\",
 }
 
 def unescape_string(s: str):
@@ -23,15 +25,30 @@ def unescape_string(s: str):
         s = s.replace(k, v)
     return s
 
+def is_valid_markdown(text: str):
+    try:
+        markdown.markdown(text)
+        return True
+    except:
+        return False
+
 def is_good_text(text):
+
+    # Not null
     if not text:
         return False
 
+    # Can be parsed to markdown
+    if not is_valid_markdown(text):
+        return False    
+
+    # No bad patterns
     bad_patterns_raw_text = [
+
         r"[0-9]+\.[0-9]+",
-        r"(给定的)|(这个)",
+        r"(给定的)|(这个)|(上述)",
         r"/(https?:\/\/)?(([0-9a-z.]+\.[a-z]+)|(([0-9]{1,3}\.){3}[0-9]{1,3}))(:[0-9]+)?(\/[0-9a-z%/.\-_]*)?(\?[0-9a-z=&%_\-]*)?(\#[0-9a-z=&%_\-]*)?/ig",
-        r"\}\}\}\}\}"
+        r"(\\boldsymbol\{){3,}",
     ]
     bad_patterns = [re.compile(pattern) for pattern in bad_patterns_raw_text]
     return all(not pattern.search(text) for pattern in bad_patterns)
