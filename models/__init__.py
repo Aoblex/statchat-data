3d5fb869a1d1c9cc888e54a1db46e94b2ef5aa91
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-30 14:36:44 +0800

    move api key to file

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,13 @@ from langchain_openai import ChatOpenAI
 class QAModel:
 
     def __init__(self):
+
+        # Read api key
+        with open("model_api_key.txt", "r") as f:
+            self.model_api_key = f.read().strip()
+
         self.model = ChatOpenAI(
-            openai_api_key="sk-n4wVb574XCbb0p3SFbF7547e4a484b3d82640c8dCe553e20",
+            openai_api_key=self.model_api_key,
             temperature=0.1,
             base_url="https://api.132999.xyz/v1",
             model="gpt-3.5-turbo-1106",
