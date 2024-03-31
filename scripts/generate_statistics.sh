42fac7606e449f4006b20f62fd05aa578dbb8f81
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-31 20:42:13 +0800

    Update scripts for generation

--- scripts/generate_statistics.sh
+++ scripts/generate_statistics.sh
@@ -6,6 +6,5 @@ python src/generate_qa.py --question_prompt_path datasets/prompts/statistics/que
                           --openai_api_key_path model_api_key.txt \
                           --temperature 0.0 \
                           --model gpt-3.5-turbo-1106 \
-                          --max_context 1 \
+                          --max_context 100 \
                           --write_frequency 1 \
-                          --random
