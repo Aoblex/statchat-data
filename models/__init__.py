c57928e9e039400b50d7fc1df46e726c297b57d4
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-31 09:20:23 +0800

    changed model parameters

--- models/__init__.py
+++ models/__init__.py
@@ -10,12 +10,12 @@ class QAModel:
 
         self.model = ChatOpenAI(
             openai_api_key=self.model_api_key,
-            temperature=0.1,
+            temperature=0.01,
             base_url="https://api.132999.xyz/v1",
             model="gpt-3.5-turbo-1106",
             model_kwargs={
-                "frequency_penalty": 0.1,
-                "presence_penalty": 0.1,
+                "frequency_penalty": 0.05,
+                "presence_penalty": 0.05,
                 "response_format": {
                     "type": "json_object",
                 },
