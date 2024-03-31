1de119a3a64ddb7f2d5ee971de36025d820d91d8
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-31 20:16:54 +0800

    Add export dataset function

--- src/statchat/data/utils.py
+++ src/statchat/data/utils.py
@@ -1,4 +1,6 @@
 from ..logging import get_logger
+import json
+import os
 import re
 
 logger = get_logger(__name__)
@@ -14,3 +16,25 @@ def remove_links(text: str) -> str:
     text = re.sub(r"\[.*?\]\(.*?\)", "", text)
     text = re.sub(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", "", text)
     return text
+
+def export_dataset(input_path: str, output_dir: str) -> None:
+    file_name = os.path.basename(input_path)
+    output_path = os.path.join(output_dir, file_name)
+    with open(input_path, "r") as f:
+        data = json.load(f)
+    
+    fine_tuning_data = []
+    for item in data:
+        questions = item['questions']
+        answers = item['answers']
+        for question, answer in zip(questions, answers):
+            fine_tuning_data.append({
+                "prompt": question,
+                "query": "",
+                "response": answer,
+                "history": [],
+            })
+
+    os.makedirs(output_dir, exist_ok=True)
+    with open(output_path, "w") as f:
+        json.dump(fine_tuning_data, f, indent=4, ensure_ascii=False)
